a2ca1666b69e73c3e5a980e1be267c7735be3187
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-07-09 14:49:45 +0200

    Create an endpoint with a get method that converts a rgb value to a hsl value

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const app = express();
 const port = 3112;
 const convert = require('color-convert');
 
+app.get('/convert/rgb-to-hsl', (req, res) => {
+    let rgbColor = req.query.color.split(',').map(colorValue => parseInt(colorValue));
+    res.send(convert.rgb.hsl(rgbColor))
+});
+
 app.listen(port, () => console.log(`ColorConverter is running on port ${port}!`));
