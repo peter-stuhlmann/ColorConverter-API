09a61013a41ae388496ee50bdcf2476d2ef54480
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-07-15 23:58:36 +0200

    Add GET request 'hsl to rgb'

--- v1/converter/converter.js
+++ v1/converter/converter.js
@@ -9,6 +9,13 @@ router.get('/rgb-to-hsl', (req, res) => {
     return res.json({ hue, saturation, luminance });
 });
 
+router.get('/hsl-to-rgb', (req, res) => {
+    const hslColor = req.query.color.split(',').map(colorValue => parseInt(colorValue));
+    const colorOutput = convert.hsl.rgb(hslColor);
+    const [red, green, blue] = colorOutput;
+    return res.json({ red, green, blue });
+});
+
 router.get('/colorname-to-rgb', (req, res) => {
     const colorOutput = convert.keyword.rgb(req.query.color);
     const [red, green, blue] = colorOutput;
